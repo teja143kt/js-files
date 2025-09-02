b10451f84c1e83b51a427a417ce35a17924122e6
Teja <tejakutchula@gmail.com>
2025-09-02 12:39:26 +0530

    Create marks.js

---
+++ marks.js
@@ -0,0 +1,17 @@
+let marks = 34; 
+if (marks < 0 || marks > 100) {
+    console.log ('invalid');
+}
+    else if (marks >= 90) {
+        console.log ('A');
+    }
+    else if (marks >= 75) {
+        console.log ('b');
+    }
+    else if (marks >= 50) {
+        console.log ('c');
+    }
+    else {
+        console.log ('fail');
+    }
+  
